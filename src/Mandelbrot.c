14fd8104e14a9f108b366fbde8162ff77d326fb7
Egakrut3 <krivovgeorgij1829@gmail.com>
2026-04-08 21:01:08 +0300

    Restructured program

--- src/Mandelbrot.c
+++ src/Mandelbrot.c
@@ -10,17 +10,14 @@
 static void error_callback(int err, char const *desc) {
 	fprintf(stderr, "GLFW Error %d: %s\n", err, desc);
 }
-struct Pixel_buffer {
+
+struct Pixel_buff {
 	GLubyte *pixels;
 	GLsizei	w,
 			h;
 };
 
-static void buff_resize_callback(GLFWwindow *win, GLsizei w, GLsizei h) {
-	struct Pixel_buffer *buff_ptr = glfwGetWindowUserPointer(win);
-	buff_ptr->w = w;
-	buff_ptr->h = h;
-	buff_ptr->pixels = realloc(buff_ptr->pixels, buff_ptr->w * buff_ptr->h * 4 * sizeof(*buff_ptr->pixels));
+static void fill_buff(struct Pixel_buff *buff_ptr) {
 	for (GLsizei y = 0; y < buff_ptr->h; y++) {
 		for (GLsizei x = 0; x < buff_ptr->w; x++) {
 			GLubyte *cur_pixel = buff_ptr->pixels + (y * buff_ptr->w + x) * 4;
@@ -31,7 +28,25 @@ static void buff_resize_callback(GLFWwindow *win, GLsizei w, GLsizei h) {
 			cur_pixel[3] = 0xFF;
 		}
 	}
+}
+
+static void update_win(GLFWwindow *win) {
+	struct Pixel_buff *buff_ptr = glfwGetWindowUserPointer(win);
+	fill_buff(buff_ptr);
+
 	glViewport(0, 0, buff_ptr->w, buff_ptr->h);
+	glDrawPixels(buff_ptr->w, buff_ptr->h, GL_RGBA, GL_UNSIGNED_BYTE, buff_ptr->pixels);
+	glfwSwapBuffers(win);
+	glDrawPixels(buff_ptr->w, buff_ptr->h, GL_RGBA, GL_UNSIGNED_BYTE, buff_ptr->pixels);
+}
+
+static void buff_resize_callback(GLFWwindow *win, GLsizei w, GLsizei h) {
+	struct Pixel_buff *buff_ptr = glfwGetWindowUserPointer(win);
+	buff_ptr->w = w;
+	buff_ptr->h = h;
+	buff_ptr->pixels = realloc(buff_ptr->pixels, buff_ptr->w * buff_ptr->h * 4 * sizeof(*buff_ptr->pixels));
+
+	update_win(win);
 }
 
 #define start_win_w	800
@@ -41,41 +56,16 @@ int run_Mandelbrot() {
 	glfwSetErrorCallback(error_callback);
 	glfwInit();
 
-
-
-	glfwWindowHint(GLFW_STEREO,				GLFW_FALSE);
-	glfwWindowHint(GLFW_DOUBLEBUFFER,			GLFW_TRUE);
-	glfwWindowHint(GLFW_CLIENT_API,			GLFW_OPENGL_API);
-
-	//glfwWindowHint(GLFW_RESIZABLE,			GLFW_FALSE);
-	// glfwWindowHint(GLFW_SCALE_FRAMEBUFFER,	GLFW_FALSE);
-
-
-
 	GLFWwindow *win = glfwCreateWindow(start_win_w, start_win_h, "FPS: ", 0, 0);
 	glfwMakeContextCurrent(win);
 	glfwSwapInterval(1);
 
-
-
-	struct Pixel_buffer buff = {};
+	struct Pixel_buff buff = {};
 	glfwGetFramebufferSize(win, &buff.w, &buff.h);
 	buff.pixels = malloc(buff.w * buff.h * 4 * sizeof(*buff.pixels));
-	for (GLsizei y = 0; y < buff.h; y++) {
-		for (GLsizei x = 0; x < buff.w; x++) {
-			GLubyte *cur_pixel = buff.pixels + (y * buff.w + x) * 4;
-			cur_pixel[0] = x		& 0xFF;
-			cur_pixel[1] = y		& 0xFF;
-			cur_pixel[2] = (x + y)	& 0xFF;
-
-			cur_pixel[3] = 0xFF;
-		}
-	}
-	glViewport(0, 0, buff.w, buff.h);
 	glfwSetWindowUserPointer(win, &buff);
+	update_win(win);
 	glfwSetFramebufferSizeCallback(win, buff_resize_callback);
-
-
 
 	#define MAX_FPS_TITLE_LENGTH 16
 	double	FPS = 0,
@@ -83,7 +73,6 @@ int run_Mandelbrot() {
 			frm_beg_time		= last_FRS_rep_time;
 	char FPS_title[MAX_FPS_TITLE_LENGTH] = "";
 	while (!glfwWindowShouldClose(win)) {
-		glDrawPixels(buff.w, buff.h, GL_RGBA, GL_UNSIGNED_BYTE, buff.pixels);
 		glfwSwapBuffers(win);
 		glfwPollEvents();
 
